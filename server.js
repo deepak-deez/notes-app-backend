8a3b264a02b1ed5089563475046f0e9948fcba18
Deepak Patra <deepak@itobuz.com>
2023-04-11 18:51:20 +0530

    updated

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
-// import  mySchema  from "./schema.js";
 import express, { response , Router } from 'express';
-// import { userData } from "./config.js";
 import noteRouter from "./modules/node-router.js";
 import cors from 'cors';
 import { connection } from "./modules/connection.js";
@@ -10,10 +8,8 @@ let port = 8085;
 let hostname = "127.0.0.1";
 app.use(express.json());
 app.use(cors());
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-//routes 
 connection()
 app.use(noteRouter)
 
@@ -32,63 +28,3 @@ app.use((error, req, res, next)=>{
   }
 })
  
-//routes
-
-
-//mongoose connection
-
-
-// async function connection() {
-//   await mongoose.connect(
-//     `mongodb+srv://${userData.userName}:${userData.password}@cluster1.hj2ru6p.mongodb.net/${userData.dbName}`
-//  ).then(() => {
-//   console.log("connected");
-// })
-// .catch((err) => console.log(err));;
-
-// }
-
-
-// connection();
-  
-
-//mongoose connection
-
-//schema
-
-// const schema = new mongoose.Schema(
-//   {
-//     notes: String,
-//   },
-//   {
-//     strict: true,
-//   }
-// );
-
-// const mySchema = mongoose.model("mySchema", schema);
-
-//schema
-
-//functions
-
-// async function create(data) {
-//   await mySchema.create(data);
-// }
-
-// async function deleteData(data) {
-//   await mySchema.findOneAndDelete(data);
-// }
-
-// async function read(data) {
-//   const notesData = await mySchema.find(data).then((myData) => {
-//     return myData;
-//   });
-//   return JSON.stringify(notesData);
-// }
-// async function update(oldData, newData) {
-//   await mySchema.findOneAndUpdate(oldData, newData);
-// }
-
-
-
-//functions
